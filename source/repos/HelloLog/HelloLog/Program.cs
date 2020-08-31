348b340ba2bf692b7b2b889b893530fc2b9e2c5a
thelittlecitizen3 <thelittlecitizen3@gmail.com>
2020-08-31 20:41:06 +0300

    massege

---
+++ source/repos/HelloLog/HelloLog/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+
+namespace HelloLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            RunLog runLog = new RunLog();
+            for (int i = 0; i < 5; i++)
+            {
+                runLog.RunLogic();
+            }
+
+        }
+    }
+}
---
+++ source/repos/HelloLog/HelloLog/RunLog.cs
@@ -0,0 +1,41 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloLog
+{
+    public class RunLog
+    {
+        public void RunLogic()
+        {
+            try
+            {
+                Random rnd = new Random();
+                int n = rnd.Next(4);
+                switch (n)
+                {
+                    case 0:
+                        Log.Information("Hello World!");
+                        break;
+                    case 1:
+                        Log.Warning("Hello World!");
+                        break;
+                    case 2:
+                        Log.Debug("Hello World!");
+                        break;
+                    case 3:
+                        Log.Error("Hello World!");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal("Hello World?" + ex.Message);
+                throw;
+            }
+        }
+    }
+}
